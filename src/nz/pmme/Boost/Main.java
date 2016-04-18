c4f6249459fcaa5762c561102cbe9484ce3c3d90
pmme <gpaulmcs@gmail.com>
2016-04-18 21:36:27 +1200

    Empty plugin

---
+++ src/nz/pmme/Boost/Main.java
@@ -0,0 +1,16 @@
+package nz.pmme.Boost;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by paul on 16-Apr-16.
+ */
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
