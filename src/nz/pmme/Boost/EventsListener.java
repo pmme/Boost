78706f3b2af5a02356033f923cf2ac540a4a6df6
pmme <gpaulmcs@gmail.com>
2016-04-22 23:25:53 +1200

    Add test using TargetBlockFinder to do block changing at distance. Also enable animal testing on boost.

--- src/nz/pmme/Boost/EventsListener.java
+++ src/nz/pmme/Boost/EventsListener.java
@@ -1,6 +1,9 @@
 package nz.pmme.Boost;
 
+import nz.pmme.Utils.TargetBlockFinder;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,19 +24,40 @@ public class EventsListener implements Listener
     }
 
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
+    public void onPlayerInteract( PlayerInteractEvent event )
+    {
+        Block targetBlock = null;
+        switch( event.getAction() ) {
+            case LEFT_CLICK_BLOCK:
+                break;
+            case RIGHT_CLICK_BLOCK:
+                targetBlock = event.getClickedBlock();
+                break;
+            case LEFT_CLICK_AIR:
+                break;
+            case RIGHT_CLICK_AIR:
+                TargetBlockFinder targetBlockFinder = new TargetBlockFinder( event.getPlayer() );
+                targetBlock = targetBlockFinder.getTargetBlock();
+                break;
+            case PHYSICAL:
+                break;
+        }
+        if( targetBlock != null ) targetBlock.setType( event.getMaterial() );
     }
 
     @EventHandler
-    public void onPlayerInteractEntity(PlayerInteractEntityEvent event)
+    public void onPlayerInteractEntity( PlayerInteractEntityEvent event )
     {
-        if(event.getRightClicked() instanceof Player) {
-            Player otherPlayer = (Player)event.getRightClicked();
+        if( event.getRightClicked() instanceof LivingEntity )
+        {
+            LivingEntity otherPlayer = (LivingEntity)event.getRightClicked();
+//            Player otherPlayer = (Player)event.getRightClicked();
             Player thisPlayer = event.getPlayer();
+
             double deltaX = otherPlayer.getLocation().getX() - thisPlayer.getLocation().getX();
             double deltaZ = otherPlayer.getLocation().getZ() - thisPlayer.getLocation().getZ();
             Vector vectorBoost = new Vector( deltaX, 3.0, deltaZ );
-            otherPlayer.setVelocity(vectorBoost);
+            otherPlayer.setVelocity( vectorBoost );
         }
     }
 }
